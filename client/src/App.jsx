48c342e79502377bf8c1e305ba7ce5e8e9074a0d
abdallah-abde <abdallah.abde@gmail.com>
2024-03-05 22:20:22 +0300

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -29,6 +30,8 @@ export default function App() {
         <Route element={<PrivateRoute isDashboard={false} />}>
           <Route path="/sign-up" element={<Signup />} />
         </Route>
+        <Route path="/search" element={<Search />} />
+
         <Route element={<PrivateRoute isDashboard={true} />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
