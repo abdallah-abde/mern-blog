19a7e97bb546a5f603f4b0d571cc0a1adedcdf55
abdallah-abde <abdallah.abde@gmail.com>
2024-03-05 22:26:56 +0300

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Abdallah's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Provident fuga itaque beatae veritatis debitis sed. Sapiente
+              exercitationem neque obcaecati possimus, ea labore corporis ad
+              beatae alias culpa natus. Vel, ad!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Provident fuga itaque beatae veritatis debitis sed. Sapiente
+              exercitationem neque obcaecati possimus, ea labore corporis ad
+              beatae alias culpa natus. Vel, ad!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Provident fuga itaque beatae veritatis debitis sed. Sapiente
+              exercitationem neque obcaecati possimus, ea labore corporis ad
+              beatae alias culpa natus. Vel, ad!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
